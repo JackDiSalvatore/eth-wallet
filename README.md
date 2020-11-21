d19659ad7ba81e4533bbedc26c34fb3191d5b17a
jackdisalvatore <jackp.disalvatore@gmail.com>
2020-11-21 11:59:20 -0600

    adding owner check and balanceOf function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ether-wallet
+
+This is a smart contract that stores Ether.  Only the "owner" of the smart contract can send Ether
+from this smart contact.
+
+[Anyone] -> "deposit" Ether -> (contract)
+[Owner] -> "send" Ether to friend -> (contract)
+(contract) -> transfer Ether to friend
+
+
--- contracts/EtherWallet.sol
+++ contracts/EtherWallet.sol
@@ -1,6 +1,12 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract EtherWallet {
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
     // "payable" keyword lets this function receive Ether
     function deposit() payable public {
     }
@@ -8,6 +14,20 @@ contract EtherWallet {
     // address "payable" keyword describes the "address" type
     // of having the the "transfer" method
     function send(address payable to, uint amount) public {
-        to.transfer(amount);
+        // "msg.sender" is the address that is sending the transaction
+        // on this smart contract
+        if (msg.sender == owner) {
+            to.transfer(amount);
+        }
+        revert('Sender is not the owner');
     }
+
+    function balanceOf() view public return (uint) {
+        // "this" keyword referce to smart contract
+        // "address(this)" transforms this smart contract
+        // into an address type
+        // ".balance" is a member variable defining the Ether balance
+        return address(this).balance;
+    }
+
 }
