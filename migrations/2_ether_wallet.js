4a7d57ddf349b0125da146965fba233a26eb3674
jackdisalvatore <jackp.disalvatore@gmail.com>
2020-11-21 12:04:02 -0600

    adding stub for contract owner in deploy migration

--- migrations/2_ether_wallet.js
+++ migrations/2_ether_wallet.js
@@ -1,6 +1,8 @@
 const EtherWallet = artifacts.require('EtherWallet')
 
+const owner = '0x0000000000000000000000000000000000000000'
+
 module.exports = function(deployer) {
-  deployer.deploy(EtherWallet)
+  deployer.deploy(EtherWallet, owner)
 }
 
